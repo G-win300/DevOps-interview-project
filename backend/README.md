464410d42c2fd19052f2b5812718f9ee5e9373e7
G-win300 <geffizy@gmail.com>
2022-10-22 03:02:52 +0100

    add files

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+SuperCool API
+This api requires uvicorn and everything listed in setup.py.
---
+++ backend/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="myapi", # Replace with your own username
+    version="0.0.1",
+    author="Frank Dekervel",
+    author_email="frank@kapernikov.com",
+    description="Test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/notyet",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'uvicorn>=0.16',
+        'fastapi>=0.63'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
